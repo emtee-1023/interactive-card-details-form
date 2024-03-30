893d2633f83e1f3cd780a02b9c4f8b3fcbdcbe0b
Mark <marktalamson@gmail.com>
2024-03-30 17:28:32 +0300

    test 2

---
+++ app/js/script.js
@@ -0,0 +1,9 @@
+const clientName = document.getElementById("name")
+const cardNumber = document.getElementById("number")
+const expiryMonth = document.getElementById("expiryMonth")
+const expiryYear = document.getElementById("expiryYear")
+const cvc = document.getElementById("cvc")
+
+const confirm = document.getElementById("confirm")
+const form = document.querySelector("form")
+
